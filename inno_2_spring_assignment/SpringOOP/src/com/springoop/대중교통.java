ce46cf6cf6698cccde471045959c9ea4f8c47bd9
Moveuk <v_donguk@naver.com>
2022-07-20 08:33:31 +0900

    add:대중교통 class 생성

---
+++ inno_2_spring_assignment/SpringOOP/src/com/springoop/대중교통.java
@@ -0,0 +1,25 @@
+package com.springoop;
+
+public class 대중교통 {
+    int 번호;
+    int 주유량 = 100;
+    int 속도 = 0;
+    int 최대승객수;
+
+
+    public void 운행_시작() {
+
+    }
+
+    public void 속도_변경() {
+
+    }
+
+    public void 상태_변경() {
+
+    }
+
+    public void 승객_탑승() {
+
+    }
+}
