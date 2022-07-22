a4551596a5c34c9177d04b4691bdfb650fd0fb7a
Moveuk <v_donguk@naver.com>
2022-07-22 18:51:00 +0900

    add: 과제 프로젝트 기본 세팅

---
+++ inno_3_spring_basic/Spring_BlogCRUD/src/main/java/com/ldu/spring_blogcrud/SpringBlogCrudApplication.java
@@ -0,0 +1,13 @@
+package com.ldu.spring_blogcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogCrudApplication.class, args);
+    }
+
+}
---
+++ inno_3_spring_basic/Spring_BlogCRUD/src/test/java/com/ldu/spring_blogcrud/SpringBlogCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldu.spring_blogcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
