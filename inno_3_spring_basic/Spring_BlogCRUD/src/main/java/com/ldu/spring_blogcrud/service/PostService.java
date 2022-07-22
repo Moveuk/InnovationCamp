c8e8d5543902f995dd468480f56c2159cedcea5a
Moveuk <v_donguk@naver.com>
2022-07-22 19:54:20 +0900

    add: 임시 PostService 생성

---
+++ inno_3_spring_basic/Spring_BlogCRUD/src/main/java/com/ldu/spring_blogcrud/service/PostService.java
@@ -0,0 +1,12 @@
+package com.ldu.spring_blogcrud.service;
+
+import com.ldu.spring_blogcrud.entity.Post;
+
+public class PostService {
+
+    public Post insertPost() {
+
+
+        return null;
+    }
+}
